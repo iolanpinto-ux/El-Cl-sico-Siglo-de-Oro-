c4948f1612b3bc884aefdca0640a895dc6faf039
Dominic <iolanpinto@gmail.com>
2026-04-17 16:50:35 -0400

    Reajuste pagina

---
+++ src/data/jugadores.ts
@@ -0,0 +1,32 @@
+export const jugadores = [
+    {
+        nombre: "Lionel Messi",
+        equipo: "FC BARCELONA",
+        imagen: "/messi.png",
+    },
+    {
+        nombre: "Raúl González",
+        equipo: "REAL MADRID",
+        imagen: "/raul.png",
+    },
+    {
+        nombre: "Ronaldo",
+        equipo: "FC BARCELONA",
+        imagen: "/ronaldinho.png",
+    },
+    {
+        nombre: "Cristiano Ronaldo",
+        equipo: "REAL MADRID",
+        imagen: "/cristiano.png",
+    },
+    {
+        nombre: "Johan Cruyff",
+        equipo: "FC BARCELONA",
+        imagen: "/johan cruyff.png",
+    },
+    {
+        nombre: "Alfredo Di Stéfano",
+        equipo: "REAL MADRID",
+        imagen: "/di stefano.png",
+    },
+];
